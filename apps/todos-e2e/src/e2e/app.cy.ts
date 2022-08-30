62d81b2b4bdf62876e2b21c421b01ee26f7e7e32
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-08-29 21:32:30 -0600

    feat: add todos

--- apps/todos-e2e/src/e2e/app.cy.ts
+++ apps/todos-e2e/src/e2e/app.cy.ts
@@ -3,14 +3,6 @@ import { getGreeting, getAddTodoButton, getTodos } from '../support/app.po';
 describe('todos', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome todos');
-  });
-
   it('should display todos', () => {
     getTodos().should((t) => expect(t.length).equal(2));
     getAddTodoButton().click();
--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,12 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { useState } from 'react';
+
+interface Todo {
+  title: string;
+}
 
 export function App() {
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'Todo 1' },
+    { title: 'Todo 2' },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      {
+        title: `New todo ${Math.floor(Math.random() * 1000)}`,
+      },
+    ]);
+  }
+
   return (
     <>
-      <NxWelcome title="todos" />
-      <div />
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'}>{t.title}</li>
+        ))}
+      </ul>
+      <button id={'add-todo'} onClick={addTodo}>
+        Add Todo
+      </button>
     </>
   );
 }
